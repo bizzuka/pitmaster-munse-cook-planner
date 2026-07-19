090f961f80439c6b9717f143593e60c8b2e5dab7
Munse <120194836+bizzuka@users.noreply.github.com>
2026-07-19 14:06:03 -0500

    Import global CSS in main.tsx

--- static/src/main.tsx
+++ static/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "../../app/globals.css";
 import Home from "../../app/page";
 
 createRoot(document.getElementById("root")!).render(
